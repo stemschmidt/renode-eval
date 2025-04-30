157c179cc2bfb11d2bbb17b5ae19b89730c4ce31
Stefan Schmidt <kontakt@stefanschmidt-embedded.de>
2025-04-30 08:12:11 +0200

    add uart analyzer

--- application/src/main.c
+++ application/src/main.c
@@ -23,6 +23,7 @@ static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 int main(void)
 {
 	int ret;
+	bool led_state = true;
 
 	if (!gpio_is_ready_dt(&led)) {
 		return 0;
@@ -39,6 +40,8 @@ int main(void)
 			return 0;
 		}
 
+		led_state = !led_state;
+		printf("LED state: %s\n", led_state ? "ON" : "OFF");
 		k_msleep(SLEEP_TIME_MS);
 	}
 	return 0;
